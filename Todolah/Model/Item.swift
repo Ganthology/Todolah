55a7be9d68a52e0ac793f045aa2a68dbd152c93f
Ganthology <gbk1386@gmail.com>
2021-07-01 02:14:53 +0800

    Create the model for Items in Table View,

---
+++ Todolah/Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todolah
+//
+//  Created by Boon Kit Gan on 01/07/2021.
+//
+
+import Foundation
+import RealmSwift
+
+class Item: Object {
+    @objc dynamic var title: String = ""
+    @objc dynamic var desc: String = ""
+    @objc dynamic var category: String = "pending"
+    @objc dynamic var deadline: Date?
+    @objc dynamic var isSelected: Bool = false
+}
